5cc0fe39325aec4fc600096ad10c6002040bbffb
chengjinqian <1048126804@qq.com>
2017-03-12 16:22:26 +0800

    搭建mybatis测试框架

--- src/com/force4us/dao/MessageDao.java
+++ src/com/force4us/dao/MessageDao.java
@@ -1,57 +1,86 @@
 package com.force4us.dao;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
 import com.force4us.bean.Message;
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
+import com.force4us.db.DBAccess;
 
 public class MessageDao {
-	List<Message> messageList = new ArrayList<Message>();
-
+	/*
+	 * mybatis方式： 根据查询条件查询消息列表
+	 */
 	public List<Message> queryMessageList(String command, String description) {
+		DBAccess dbAccess = new DBAccess();
+		SqlSession sqlSession = null;
+
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			try {
-				Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/micro_message",
-						"root", "2011210847");
-				StringBuilder sql = new StringBuilder("select ID,COMMAND,DESCRIPTION,CONTENT from message where 1=1");
-				List<String> paramList = new ArrayList<String>();
-				if (command != null && !"".equals(command.trim())) {
-					sql.append(" and COMMAND=?");
-					paramList.add(command);
-				}
-				if (description != null && !"".equals(description.trim())) {
-					sql.append(" and DESCRIPTION like '%' ? '%'");
-					paramList.add(description);
-				}
-				PreparedStatement statement = (PreparedStatement) conn.prepareStatement(sql.toString());
-				for (int i = 0; i < paramList.size(); i++) {
-					statement.setString(i + 1, paramList.get(i));
-				}
-				ResultSet rs = statement.executeQuery();
+			sqlSession = dbAccess.getSqlSession();
+			// 通过sqlSession执行SQL语句
 
-				while (rs.next()) {
-					Message message = new Message();
-					messageList.add(message);
-					message.setId(rs.getString("ID"));
-					message.setCommand(rs.getString("COMMAND"));
-					message.setDescription(rs.getString("DESCRIPTION"));
-					message.setContent(rs.getString("CONTENT"));
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
 		}
-
-		return messageList;
+		return null;
 	}
+
+	public static void main(String[] args) {
+		MessageDao messageDao = new MessageDao();
+		messageDao.queryMessageList("", "");
+	}
+	/*
+	 * 传统jdbc方式：根据查询条件查询消息列表
+	 */
+	// public List<Message> queryMessageList(String command, String description)
+	// {
+	// List<Message> messageList = new ArrayList<Message>();
+	// try {
+	// Class.forName("com.mysql.jdbc.Driver");
+	// try {
+	// Connection conn = (Connection)
+	// DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/micro_message",
+	// "root", "2011210847");
+	// StringBuilder sql = new StringBuilder("select
+	// ID,COMMAND,DESCRIPTION,CONTENT from message where 1=1");
+	// List<String> paramList = new ArrayList<String>();
+	// if (command != null && !"".equals(command.trim())) {
+	// sql.append(" and COMMAND=?");
+	// paramList.add(command);
+	// }
+	// if (description != null && !"".equals(description.trim())) {
+	// sql.append(" and DESCRIPTION like '%' ? '%'");
+	// paramList.add(description);
+	// }
+	// PreparedStatement statement = (PreparedStatement)
+	// conn.prepareStatement(sql.toString());
+	// for (int i = 0; i < paramList.size(); i++) {
+	// statement.setString(i + 1, paramList.get(i));
+	// }
+	// ResultSet rs = statement.executeQuery();
+	//
+	// while (rs.next()) {
+	// Message message = new Message();
+	// messageList.add(message);
+	// message.setId(rs.getString("ID"));
+	// message.setCommand(rs.getString("COMMAND"));
+	// message.setDescription(rs.getString("DESCRIPTION"));
+	// message.setContent(rs.getString("CONTENT"));
+	// }
+	// } catch (SQLException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// } catch (ClassNotFoundException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// return messageList;
+	// }
 }
